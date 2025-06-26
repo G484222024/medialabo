c57813ab128b4cfd19d89e52613c1ab88915314a
G484222024 <248422@st.takushoku-u.ac.jp>
2025-06-26 15:23:22 +0900

    4-9

--- practice-eventdom/input-demo.js
+++ practice-eventdom/input-demo.js
@@ -1,5 +1,9 @@
 b = document.querySelector('button#print');
-b.addEventListener('click', greeting);
-function greeting() {
-    console.log('こんにちは');
-}
+b.addEventListener('click', greeting2);
+function greeting2() {
+
+  let i = document.querySelector("input[name=shimei]");
+    let name = i.value;
+    let p = document.querySelector('p#message');
+    p.textContent = 'こんにちは、'+ name + 'さん';
+   }
